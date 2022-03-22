088b8f4aafa3393f12ef132096f8f93af5df97f3
Naoto Kondo <cgigcp3yqt@gmail.com>
2022-03-22 18:00:29 +0900

    fix python 3.7 issue on windows

--- NDIlib/__init__.py
+++ NDIlib/__init__.py
@@ -1,6 +1,7 @@
 import os
+import sys
 
-if os.name == 'nt':
+if os.name == 'nt' and sys.version_info.major >= 3 and sys.version_info.minor >= 8:
     os.add_dll_directory(os.path.dirname(__file__))
 
 from .NDIlib import *
