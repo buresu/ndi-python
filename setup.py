b4bad8caade929ed0e9e066dfeda86fdb698e834
Naoto Kondo <cgigcp3yqt@gmail.com>
2022-05-18 19:56:56 +0900

    version 5.1.1.4

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ long_description = (this_directory / "README.md").read_text()
 # setup
 setup(
     name='ndi-python',
-    version='5.1.1.3',
+    version='5.1.1.4',
     description='Wrapper package for NDI SDK python bindings.',
     long_description=long_description,
     long_description_content_type='text/markdown',
